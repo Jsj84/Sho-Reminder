f7a478add6a5ff988384e9b0f05f91eef0e2b4bf
Jesse St. John <jessestjohn1@macbookpro-63be.omnicomgroup.com>
2017-01-06 15:58:41 -0500

    f

--- TimeViewController.swift
+++ TimeViewController.swift
@@ -25,6 +25,19 @@ class TimeViewController: UIViewController, UITableViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Items")
+        
+        do {
+            cellTitles = try managedContext.fetch(fetchRequest)
+            dateCell = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    
         view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
         
         tableView.delegate = self
@@ -38,54 +51,27 @@ class TimeViewController: UIViewController, UITableViewDelegate {
         
         
     }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        //1
-        guard let appDelegate =
-            UIApplication.shared.delegate as? AppDelegate else {
-                return
-        }
-        
-        let managedContext =
-            appDelegate.persistentContainer.viewContext
-        
-        //2
-        let fetchRequest =
-            NSFetchRequest<NSManagedObject>(entityName: "Items")
-        
-        //3
-        do {
-            cellTitles = try managedContext.fetch(fetchRequest)
-            dateCell = try managedContext.fetch(fetchRequest)
-        } catch let error as NSError {
-            print("Could not fetch. \(error), \(error.userInfo)")
-        }
-    }
-
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-//            let nameToDelete = indexPath.row
-//            let dateToDelete = indexPath.row
-//            fh.deleteRecords(name: nameToDelete, date: dateToDelete)
-//            fh.names.remove(at: indexPath.row)
-//            fh.date.remove(at: indexPath.row)
-//            tableView.reloadData()
-//        }
-//    }
 }
 extension TimeViewController: UITableViewDataSource {
      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return cellTitles.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let rowTitle = cellTitles[indexPath.row]
-        let rowDate = dateCell[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TimeTableViewCell
-        cell.myLabel_1.text = rowTitle.value(forKey: "name") as! String?
-        cell.myLabel_2.text = rowDate.value(forKey: "dateString") as! String?
+        cell.myLabel_1.text = cellTitles[indexPath.row].value(forKey: "name") as! String?
+        cell.myLabel_2.text = dateCell[indexPath.row ].value(forKey: "dateString") as! String?
         return cell 
     }
+    //    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    //        if editingStyle == .delete {
+    //            let nameToDelete = indexPath.row
+    //            let dateToDelete = indexPath.row
+    //            fh.deleteRecords(name: nameToDelete, date: dateToDelete)
+    //            fh.names.remove(at: indexPath.row)
+    //            fh.date.remove(at: indexPath.row)
+    //            tableView.reloadData()
+    //        }
+    //    }
 }
 extension UIViewController {
     func hideKeyboardWhenTappedAround() {
